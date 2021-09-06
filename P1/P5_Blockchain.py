129d3ac3937b1a8b5a5481dff7844b1af9070775
nendale <nahumtefera@gmail.com>
2021-09-06 11:58:32 -0700

    finished blockchain problem

---
+++ P1/P5_Blockchain.py
@@ -0,0 +1,58 @@
+import hashlib
+import datetime  
+
+class Block:
+
+    def __init__(self, timestamp, data, previous_hash):
+      self.timestamp = timestamp
+      self.data = data
+      self.next = None
+      self.index = 0
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash()
+
+    def calc_hash(self):
+      sha = hashlib.sha256()
+      hash_str = self.data.encode('utf-8') + str(self.timestamp).encode('utf-8') + str(self.previous_hash).encode('utf-8')
+      sha.update(hash_str)
+      return sha.hexdigest()
+
+    def set_index(self, index):
+      self.index = index
+    
+    def set_next(self, block):
+      self.next = block
+
+
+class BlockChain:
+  def __init__(self):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
+    genesis_block = Block(timestamp, "Genesis Block", 0)
+    self.head = genesis_block
+    self.tail = genesis_block
+    self.order = list([genesis_block])
+    self.size = 1
+  
+  def append(self, data):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
+    new_block = Block(timestamp, data, self.tail.hash)
+    self.tail.next = new_block
+    self.tail = new_block
+    self.order.append(new_block)
+  
+    
+
+new_block_chain = BlockChain()
+
+new_block_chain.append('paying luke $10')
+new_block_chain.append('paying jona $40')
+new_block_chain.append('$50 deposited')
+
+block = new_block_chain.head
+
+while block:
+  print('index', block.index)
+  print('data', block.data)
+  print('hash', block.hash)
+  print('prev_hash', block.previous_hash)
+  block = block.next
